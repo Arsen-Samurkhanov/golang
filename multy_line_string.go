84a754573d657bc1c12e20a490b070a5640bf45f
arsen <you@example.com>
2025-03-06 19:36:47 +0300

    strings

---
+++ multy_line_string.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(
+		`
+		This is
+		multi line
+		String
+		`)
+}
---
+++ tab_and_newline.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("1.\tSahrk\n2.\tShrimp\n3.\tSquid")
+}
